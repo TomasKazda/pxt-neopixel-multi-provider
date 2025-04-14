ea6eb32c042a144eeb8a57e63b53603c2278d84c
Tomáš Kazda <36702826+TomasKazda@users.noreply.github.com>
2025-04-14 10:14:11 +0200

    create Kazda namespace

--- main.ts
+++ main.ts
@@ -12,6 +12,7 @@ const startOnce = (stripLength: number): void => {
     providers = [
         NameTemplate.provider,
         Novakovic.provider,
+        Kazda.provider
     ];
 
     for (let provider of providers) {
